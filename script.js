805246a87cd97467840aafdf98160b8dd269ffb4
severinbunse <bunses@newschool.edu>
2025-09-05 15:57:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,26 @@ nameItems.forEach((item) => {
     const audio = new Audio(item.dataset.audio);
     audio.preload = 'auto';
 
-    // When finger touches the name
+    const link = item.querySelector('a');
+
+    // Touch start: play audio + apply hover style
     item.addEventListener('touchstart', (e) => {
         audio.currentTime = 0;
         audio.play().catch(() => {});
+
+        // Add hover effect
+        link.style.fontStyle = 'italic';
     });
 
-    // When finger is lifted
+    // Touch end: stop audio + remove hover + navigate
     item.addEventListener('touchend', (e) => {
         audio.pause();
         audio.currentTime = 0;
 
-        // Navigate to the link inside this item
-        const link = item.querySelector('a');
+        // Remove hover effect
+        link.style.fontStyle = '';
+
+        // Navigate to link
         if (link && link.href) {
             window.location = link.href;
         }
